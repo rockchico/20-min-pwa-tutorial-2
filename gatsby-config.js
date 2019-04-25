b58de13393736e180ed06715f78f614a6cfa978f
Francisco Schwertner <rockchico@gmail.com>
2019-04-25 15:58:40 -0300

    ajuste prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/20-min-pwa-tutorial-2",
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -21,8 +22,7 @@ module.exports = {
       options: {
         name: `gatsby-starter-default`,
         short_name: `starter`,
-        //start_url: `/`,
-        start_url: `20-min-pwa-tutorial-2`,
+        start_url: `/20-min-pwa-tutorial-2`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
